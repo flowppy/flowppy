164afbf74de9e692a9f237797afbab31f74f7140
Natinusala <natinusala@gmail.com>
2015-10-02 17:19:27 +0000

    Détection de opdis au démarrage

--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
+# -*- coding: utf-8 -*-
+
+#Imports
+import shutil;
+
+#Main
 def main():
+    #Vérification de la présence d'opdis dans le PATH
+    if (shutil.which("opdis") is None):
+        print("Impossible de trouver opdis, est-il installé ?");
+        return;
+        
     return;
+
+
 
 if __name__ == "__main__":
     main();
