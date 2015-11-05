2f1e40a5b47fa065597af6c7d120fb54e7cd967c
Nathan S <natinusala@gmail.com>
2015-11-05 09:44:27 +0100

    ajout des jumps de x86

--- src/GraphDriver.py
+++ src/GraphDriver.py
@@ -3,9 +3,11 @@
 from abc import ABCMeta, abstractmethod
 
 class GraphDriver:
+
+    JUMPS_X86 = ["jo", "jno", "js", "jns", "je", "jz", "jne", "jnz", "jb", "jnae", "jc", "jnb", "jae", "jnc", "jbe", "jna", "ja", "jnbe", "jl", "jnge", "jge", "jnl", "jle", "jng", "jg", "jnle", "jp", "jpe", "jlp", "jpo", "jcxz", "jecxz"];
     
     def __init__(self, outputmanager):
-        __metaclass__ = ABCMeta
+        __metaclass__ = ABCMeta;
     
         self.outputManager = outputmanager;
         
