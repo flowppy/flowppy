d0a303b48a7dcd0f41f412f55c7f1580e120ebe6
maveist <maveist44@gmail.com>
2015-11-09 16:04:02 +0100

    create graph condensed + bloc class

---
+++ src/Bloc.py
@@ -0,0 +1,13 @@
+class Bloc:
+    
+    def __init__(self, vma):
+        self.etiquette = vma;
+        self.instruction = [];
+        
+        
+    def setSon(bloc):
+        self.blocSon = bloc; 
+        
+    def addInstruciton(instruction):
+        self.instruction.append(instruction);
+        
--- src/CondensedDriver.py
+++ src/CondensedDriver.py
@@ -9,4 +9,26 @@ class CondensedDriver(GraphDriver.GraphDriver):
         return "condensed";
         
     def create_graph(self, instructions_table, vma_instructions_table):
+        waiting_list = [];
+        i = 0;
+        bloc = Bloc(intruction_table[i].vma);
+        while(i <= len(instructions_table)-1):
+            i++;
+            cur_inst =instruction_table[i];
+            if(is_jump(self, cur_inst)):
+                bloc2 = Bloc(cur_inst.vma);
+                bloc.setSon(bloc2);
+                waiting_list.append(bloc2.etiquette : bloc2);
+            else:
+                if(cur_inst.vma in waiting_list):
+                    bloc2 = waiting_list[cur_inst.vma];
+                    bloc.addSon(bloc2);
+                    bloc = bloc2;
+                else:
+                    if(cur_inst.ascii is "retq"):
+                        bloc2 = Bloc(cur_inst.vma);
+                        bloc.addSon(bloc2);
+                        bloc = bloc2;
+                    else:
+                        bloc.addInstruction(cur_inst.create_string());
         pass;
