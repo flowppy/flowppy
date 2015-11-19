b31c56223ef8ee39d5766d3bc5ff0c79c42fae49
Natinusala <natinusala@gmail.com>
2015-11-19 11:02:24 +0000

    driver condensé terminé

--- src/Bloc.py
+++ src/Bloc.py
@@ -4,7 +4,9 @@ class Bloc(object):
     
     def __init__(self, vma):
         self.etiquette = vma;
-        self.instruction = "";
+        self.instruction = [];
+        self.passedR = False;
+        self.passedL = False;
         
         self.blocSonLeft = None;
         self.blocSonRight = None;
@@ -16,15 +18,70 @@ class Bloc(object):
             self.blocSonRight = bloc;
         
     def addInstruction(self, instruction):
-        self.instruction = self.instruction + instruction.create_string() + "\n";
+        self.instruction.append(instruction);
+        
+    def containsInstruction(self, instruction):
+        for xx in self.instruction:
+            if(int(xx.vma,0) == int(instruction,0)):
+                return True;
+        return False;
+    
+    def getParent(self, bloc_nimp):
+        if self.blocSonLeft == bloc_nimp:
+            return self;
+        if self.blocSonRight == bloc_nimp:
+            return self;
+        else:
+            if self.blocSonLeft is not None:
+                return self.blocSonLeft.getParent(bloc_nimp);
+            if self.blocSonRight is not None:
+                return self.blocSonRight.getParent(bloc_nimp);
+            return self;
+            
+    def getBloc(self, instruction):
+        if(self.containsInstruction(instruction)):
+            return self;
+        else:
+            if self.blocSonLeft is not None:
+                return self.blocSonLeft.getBloc(instruction)
+            if self.blocSonRight is not None:
+                return self.blocSonRight.getBloc(instruction)
+            return self;
+            
+    def getSon(self):
+            if self.blocSonLeft is not None:
+                return self.blocSonLeft;
+            if self.blocSonRight is not None:
+                return self.blocSonRight;
+            return self;
+
+
+    def instructionStr(self):
+        str = "";
+        for inst in self.instruction:
+            str = str  + inst.create_string()+ "\l";
+        return str;
+    def replaceSon(self, bloc1, bloc2):
+        if self.blocSonLeft == bloc2:
+            self.blocSonLeft = bloc1; 
+        elif self.blocSonRight == bloc2:
+            self.blocSonRight = bloc1;
+        elif self.blocSonLeft is None:
+            self.blocSonLeft = bloc1; 
+        elif self.blocSonRight is None:
+            self.blocSonRight = bloc1;
         
     def get_graph(self, graph):
-        if self.blocSonLeft is not None:
+        #Passed mark
+        if self.blocSonLeft is not None and not self.passedL:
+            self.passedL = True;
             graph = self.blocSonLeft.get_graph(graph);
-            graph.add_edge(self.instruction, self.blocSonLeft.instruction);
-        if self.blocSonRight is not None:
+            graph.add_edge(self.instructionStr(), self.blocSonLeft.instructionStr());
+        if self.blocSonRight is not None and not self.passedR:
+            self.passedR = True;
             graph = self.blocSonRight.get_graph(graph);
-            graph.add_edge(self.instruction, self.blocSonRight.instruction);
+            graph.add_edge(self.instructionStr(), self.blocSonRight.instructionStr());
+        
             
         return graph;
         
--- src/CondensedDriver.py
+++ src/CondensedDriver.py
@@ -3,6 +3,8 @@
 import GraphDriver;
 import networkx as nx;
 import Bloc;
+import sys
+sys.setrecursionlimit(10000);
 
 class CondensedDriver(GraphDriver.GraphDriver):
     
@@ -12,32 +14,58 @@ class CondensedDriver(GraphDriver.GraphDriver):
     def create_graph(self, instructions_table, vma_instructions_table):
         waiting_list = {};
         i = 0;
-        bloc = Bloc.Bloc(instructions_table[i].vma);
-        bloc_origin = bloc;
+        bloc_cur = Bloc.Bloc(instructions_table[i].vma);
+        bloc_origin = bloc_cur;
+        
         while(i <= len(instructions_table)-1):
             cur_inst = instructions_table[i];
-            if(super(CondensedDriver, self).is_jump(cur_inst)):
-                bloc2 = Bloc.Bloc(cur_inst.vma);
-                bloc.addSon(bloc2);
-                waiting_list[int(bloc2.etiquette, 0)] = bloc2;
-            else:
-                if(cur_inst.vma in waiting_list):
-                    bloc2 = waiting_list[cur_inst.vma];
-                    bloc.addSon(bloc2);
-                    bloc = bloc2;
-                else:
-                    if(cur_inst.ascii is "retq"): #TODO Changer le retq
-                        bloc2 = Bloc.Bloc(cur_inst.vma);
-                        bloc.addSon(bloc2);
-                        bloc = bloc2;
-                    else:
-                        bloc.addInstruction(cur_inst);
-            i = i+1;
             
+            
+            if(super(CondensedDriver, self).is_jump(cur_inst)):
+                #condition pour faire remonter l'adresse cible d'un jump vers un bloc déjà créé
+                if(int(cur_inst.operands[0].ascii, 0)<int(cur_inst.vma,0)):#
+                    bloc_cur.addInstruction(cur_inst);
+                    bloc_tmp = bloc_origin.getBloc(cur_inst.operands[0].ascii);
+                    j=0;
+                    print(j);
+                    print(bloc_tmp.instruction[j]);
+                    Bloc_mid1 = Bloc.Bloc(bloc_tmp.instruction[j].vma);
+                    while j<len(bloc_tmp.instruction):
+                        Bloc_mid1.addInstruction(bloc_tmp.instruction[j]);
+                        j=j+1;
+                    bloc_origin.getParent(bloc_tmp).replaceSon(Bloc_mid1, bloc_tmp);
+                    print(Bloc_mid1.instructionStr());
+                    bloc_cur.addSon(Bloc_mid1);
+                    Bloc_mid1.addSon(bloc_tmp.getSon());
+                    Bloc_tmp=Bloc.Bloc(instructions_table[i+1]);
+                    bloc_cur.addSon(Bloc_tmp);
+                    bloc_cur=Bloc_tmp;
+                else:
+                    bloc_cur.addInstruction(cur_inst);
+                    Bloc_tmp=Bloc.Bloc( instructions_table[i]);
+                    bloc_cur.addSon(Bloc_tmp);
+                    #print(int(cur_inst.operands[0].ascii, 0));
+                    waiting_list[int(cur_inst.operands[0].ascii, 0)] = bloc_cur;
+                    bloc_cur = Bloc_tmp;
+              
+            else:
+                if(int(cur_inst.vma,0) in waiting_list):
+                    #print(waiting_list[int(cur_inst.vma, 0)]);
+                    bloc_tmp=Bloc.Bloc(cur_inst);
+                    waiting_list[int(cur_inst.vma, 0)].addSon(bloc_tmp);
+                    bloc_cur.addSon(bloc_tmp);
+                    bloc_cur = bloc_tmp;
+                else:
+                    bloc_cur.addInstruction(cur_inst);
+           
+            i = i+1;
         graph = nx.DiGraph();
+        #essai avec historique
+        historique = [];
         graph = bloc_origin.get_graph(graph);
         
         return graph;
+
 
 
 
