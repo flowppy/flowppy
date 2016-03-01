3e4acc909bef4b35deae36c7ec1819bd7981a175
Natinusala <natinusala@gmail.com>
2016-03-01 14:21:01 +0000

    correction du json

--- src/JsonMaker.py
+++ src/JsonMaker.py
@@ -3,7 +3,7 @@ def to_json(graph, output_file, graph_type):
     json = "";
     nodes = graph.node;
     edges = graph.edges(data = True);
-    json = json + "nodes : [ \n";
+    json = json + "\"nodes\" : [ \n";
     cpt = 1;
     for node in nodes:
         if graph_type == "condensed":
@@ -11,24 +11,27 @@ def to_json(graph, output_file, graph_type):
             node_str = node_str[:len(node)-1]+'"\n';
         else:
             node_str = node[:len(node)-2]+'\\n"';
-        str_buff = "{id:"+str(cpt)+", label:"+node_str+"},  \n";
+        str_buff = "{\"id\":"+str(cpt)+", \"label\":"+node_str+"},\n";
         nodes_id[node] = cpt;
         json = json + str_buff;
         cpt = cpt+1;
+    json = json[:-2];
     json = json + "],\n";
     
     one_edge = scan_edges(graph, nodes_id);
     
-    json = json + "edges : [\n";
+    json = json + "\"edges\" : [\n";
     for edge in edges:
-        str_buff = "{from: "+str(nodes_id[edge[0]])+", to: "+str(nodes_id[edge[1]]);
+        str_buff = "{\"from\": "+str(nodes_id[edge[0]])+", \"to\": "+str(nodes_id[edge[1]]);
         if(edge[2]['label'] != '""'):
-            str_buff = str_buff + ', label : '+edge[2]['label'];
+            str_buff = str_buff + ', \"label\" : '+edge[2]['label'];
         if (nodes_id[edge[0]] in one_edge or nodes_id[edge[1]] in one_edge):
-            str_buff = str_buff + ",	smooth: { enabled: false }";
+            str_buff = str_buff + ",	\"smooth\": { \"enabled\": false }";
         str_buff = str_buff + "},\n";
         json = json + str_buff;
+    json = json[:-2];
     json = json + "]";
+    json = json.replace("\>", ">").replace("\<", "<");
     if output_file != "":
         json_file = open(output_file, "w");
         json_file.write(json);
